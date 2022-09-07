515908a8f39495b04d99f3736a0a442b03e0371d
Valentino Medimorec <valentino@vallic.com>
2022-09-08 00:24:01 +0200

    Create Github action

--- build.sh
+++ build.sh
@@ -30,22 +30,24 @@ set -e
 ###############################################################################
 
 # Flag if we want to use this script to check for version.
-APP_VERSION_CHECK=$1
-APP_VERSION_ONLY=$2
+GITHUB_RUNNING_ACTION=${GITHUB_ACTIONS:-false}
 
-if [ -z "$1" ]
-  then
-    APP_VERSION_CHECK='force';
+if [ "$GITHUB_RUNNING_ACTION" == false ]; then
+  INPUT_VERSION_CHECK=$1
+  INPUT_VERSION_ONLY=$2
 fi
 
-if [ -z "$2" ]
-  then
-    APP_VERSION_ONLY='update';
+if [ -z "$1" ]; then
+  INPUT_VERSION_CHECK='force'
+fi
+
+if [ -z "$2" ]; then
+  INPUT_VERSION_ONLY='update'
 fi
 
 # Get GitHub user and repo.
-GH_USER="$( echo "$GITHUB_REPOSITORY" | grep -o ".*/" | head -c-2 )"
-GH_REPO="$( echo "$GITHUB_REPOSITORY" | grep -o "/.*" | cut -c2- )"
+GH_USER="$(echo "$GITHUB_REPOSITORY" | grep -o ".*/" | head -c-2)"
+GH_REPO="$(echo "$GITHUB_REPOSITORY" | grep -o "/.*" | cut -c2-)"
 
 echo "==> Setup default variables"
 # Setup defaults.
@@ -60,24 +62,31 @@ mkdir -p $APP_DIRECTORY/usr/share/icons/hicolor/{128x128,256x256,512x512}/apps/
 APP_FILENAME=app.desktop
 APP_SHORT_NAME=$(sed -n 's/^Name=//p' $APP_FILENAME | head -1)
 APP_ICON=$(sed -n 's/^Icon=//p' $APP_FILENAME | head -1)
+APP_EXEC=$(sed -n 's/^Exec=//p' $APP_FILENAME | head -1 | cut -d " " -f 1)
+APP_NAME=$(sed -n 's/^GenericName=//p' $APP_FILENAME)
 APP_DOWNLOAD_URL=$(sed -n 's/^VersionUrl=//p' $APP_FILENAME)
 APP_VERSION_FILE=$(sed -n 's/^VersionFile=//p' $APP_FILENAME)
 APP_VERSION_BASH=$(sed -n 's/^VersionBash=//p' $APP_FILENAME)
-APP_EXEC=$(sed -n 's/^Exec=//p' $APP_FILENAME | head -1 | cut -d " " -f 1)
-APP_NAME=$(sed -n 's/^GenericName=//p' $APP_FILENAME)
 APP_VERSION_ICON=$(sed -n 's/^VersionIcon=//p' $APP_FILENAME)
 APP_DEPLOY=$(sed -n 's/^VersionDirectory=//p' $APP_FILENAME)
 
+# For GitHub actions use inputs.
+if [ "$GITHUB_RUNNING_ACTION" == true ]
+then
+  APP_DOWNLOAD_URL=${INPUT_VERSION_URL}
+  APP_VERSION_FILE=${INPUT_VERSION_FILE}
+  APP_VERSION_BASH=${INPUT_VERSION_BASH}
+  APP_VERSION_ICON=${INPUT_VERSION_ICON}
+  APP_DEPLOY=${INPUT_VERSION_DIRECTORY}
+fi
+
 if [ -z "$APP_DEPLOY" ]; then
-    APP_DEPLOY=$BIN_DIRECTORY
+  APP_DEPLOY=$BIN_DIRECTORY
 fi
 
 if [ -z "$APP_NAME" ]; then
-    APP_NAME=$APP_SHORT_NAME
+  APP_NAME=$APP_SHORT_NAME
 fi
-
-echo "APP_NAME=$APP_NAME" >> "$GITHUB_ENV"
-echo "APP_SHORT_NAME=$APP_SHORT_NAME" >> "$GITHUB_ENV"
 
 echo "==> Download $APP_SHORT_NAME"
 wget -O "$APP_SHORT_NAME".tar.gz "$APP_DOWNLOAD_URL"
@@ -86,7 +95,7 @@ echo "==> Extract $APP_SHORT_NAME"
 tar -xzvf "$APP_SHORT_NAME".tar.gz --strip-components=1 -C $APP_DEPLOY && rm -r *.tar.gz
 
 echo "==> Check Version $APP_SHORT_NAME"
-PACKAGE=$(cat $APP_DEPLOY/"$APP_VERSION_FILE")
+PACKAGE=$(cat "$APP_DEPLOY"/"$APP_VERSION_FILE")
 
 # Default search for version inside version file, otherwise run specified bash.
 if [ -z "$APP_VERSION_BASH" ]; then
@@ -95,40 +104,36 @@ else
   VERSION=$(echo "$PACKAGE" | $APP_VERSION_BASH)
 fi
 
-echo "APP_VERSION=$VERSION" >> "$GITHUB_ENV"
-
 # If we check only for version here.
-if [ "$APP_VERSION_CHECK" == 'verify' ]
-  then
-    RELEASE_VERSION=$(gh api -H "Accept: application/vnd.github+json" /repos/"$GH_USER"/"$GH_REPO"/releases/latest | jq -r  ".name" | sed 's/'"$APP_NAME"' AppImage //g')
+if [ "$INPUT_VERSION_CHECK" == 'verify' ] && [ "$GITHUB_RUNNING_ACTION" == true ]; then
+  RELEASE_VERSION=$(gh api -H "Accept: application/vnd.github+json" /repos/"$GH_USER"/"$GH_REPO"/releases/latest | jq -r ".name" | sed 's/'"$APP_NAME"' AppImage //g')
 
-    if [ "$VERSION" = "$RELEASE_VERSION" ]; then
-        echo "::set-output name=create::false"
-        echo "APP_UPDATE_NEEDED=0" >> "$GITHUB_ENV"
-        # Always exit here.
-        echo "No update needed. Exiting."
-        exit 0
-    else
-        echo "::set-output name=create::true"
-        echo "APP_UPDATE_NEEDED=1" >> "$GITHUB_ENV"
-    fi
+  if [ "$VERSION" = "$RELEASE_VERSION" ]; then
+    echo "::set-output name=app_update_needed::false"
+    echo "APP_UPDATE_NEEDED=0" >>"$GITHUB_ENV"
+    # Always exit here.
+    echo "No update needed. Exiting."
+    exit 0
+  else
+    echo "::set-output name=app_update_needed::true"
+    echo "APP_UPDATE_NEEDED=1" >>"$GITHUB_ENV"
+  fi
 
-   # Exit if there is separate logic for checking version and building AppImage.
-   if [ "$APP_VERSION_ONLY" == 'version-only' ]
-     then
-        # If we need to check version only, return 0 as success.
-        echo "Exiting, explicitly requested"
-        exit 0
-     fi
+  # Exit if there is separate logic for checking version and building AppImage.
+  if [ "$INPUT_VERSION_ONLY" == 'version-only' ]; then
+    # If we need to check version only, return 0 as success.
+    echo "Exiting, explicitly requested"
+    exit 0
+  fi
 fi
 
 echo "==> Check binary $APP_SHORT_NAME"
 if [ -f "$BIN_DIRECTORY/$APP_EXEC" ]; then
-    echo "Binary exists."
+  echo "Binary exists."
 else
-    echo "Creating symlink does not exist."
-    BIN_PATH=$(find $APP_DEPLOY -type f -name "$APP_EXEC")
-    ln -s "$BIN_PATH" $BIN_DIRECTORY/"$APP_EXEC"
+  echo "Creating symlink does not exist."
+  BIN_PATH=$(find $APP_DEPLOY -type f -name "$APP_EXEC")
+  ln -s "$BIN_PATH" $BIN_DIRECTORY/"$APP_EXEC"
 fi
 
 echo "==> Fetch default AppRun binary"
@@ -156,6 +161,17 @@ echo "==> Build $APP_SHORT_NAME AppImage"
 # Fetch AppImageTool.
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod +x *.AppImage
-ARCH=x86_64 ./appimagetool-x86_64.AppImage --comp gzip "$APP_DIRECTORY" -n -u "gh-releases-zsync|$GH_USER|$GH_REPO|latest|$APP_SHORT_NAME*.AppImage.zsync"
+
+if [ "$GITHUB_RUNNING_ACTION" == true ]; then
+  {
+    echo "APP_NAME=$APP_NAME"
+    "APP_SHORT_NAME=$APP_SHORT_NAME"
+    "APP_VERSION=$VERSION"
+  } >>"$GITHUB_ENV"
+  ARCH=x86_64 ./appimagetool-x86_64.AppImage --comp gzip "$APP_DIRECTORY" -n -u "gh-releases-zsync|$GH_USER|$GH_REPO|latest|$APP_SHORT_NAME*.AppImage.zsync"
+else
+  ARCH=x86_64 ./appimagetool-x86_64.AppImage --comp gzip "$APP_DIRECTORY" -n
+fi
+
 mkdir dist
 mv "$APP_SHORT_NAME"*.AppImage* dist/.
